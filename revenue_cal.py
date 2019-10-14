0a841a8edfba98af1bfb9c7b96e4d74dc2c19988
starbowie <55412889+starbowie@users.noreply.github.com>
2019-10-13 21:47:30 -0400

    defined function to cal revenues

--- revenue_cal.py
+++ revenue_cal.py
@@ -1,15 +1,27 @@
 import pandas as pd
 import datetime
+import matplotlib.pyplot as plt
 
-data = pd.read_csv('matilda_files/Basic.txt', header=None)
-df = pd.DataFrame(data)
-tdelta = datetime.timedelta(days=0)
-tday = datetime.date.today()
-dates = [0] * data.size
-for i in range(data.size, 1, -1):
-    date = tday - tdelta
-    dates[i - 1] = date
-    tdelta = datetime.timedelta(days=1)
-    tday = date
-df['Dates'] = dates
-print(df)
+
+# basic_uri = 'matilda_files/Basic.txt'
+# delux_uri = 'matilda_files/Delux.txt'
+# total_uri = 'matilda_files/Total.txt'
+
+def revenue_graphs(uri):
+    data = pd.read_csv(uri)
+    df = pd.DataFrame(data)
+    tdelta = datetime.timedelta(days=int(data.size - 1))
+    tday = datetime.date.today()
+    start = tday - tdelta
+    ph = 'Total:'
+    if "Basic" in uri:
+        ph = 'Basic Cupcake:'
+    elif "Delux" in uri:
+        ph = 'Delux Cupcakes:'
+    df['Dates'] = pd.Series(pd.date_range(start, freq='D', periods=data.size))
+    df_yearly = pd.DataFrame(df.groupby([pd.Grouper(key='Dates', freq='Y')])[ph].sum().reset_index())
+    df_monthly = pd.DataFrame(df.groupby([pd.Grouper(key='Dates', freq='M')])[ph].sum().reset_index())
+    df_weekly = pd.DataFrame(df.groupby([pd.Grouper(key='Dates', freq='W')])[ph].sum().reset_index())
+    df_yearly.plot.bar(x='Dates', y=ph)
+    df_monthly.plot.bar(x='Dates', y=ph)
+    df_weekly.plot.bar(x='Dates', y=ph)
---
+++ revenue_graphs.py
@@ -0,0 +1,9 @@
+from revenue_cal import revenue_graphs
+
+basic_uri = 'matilda_files/Basic.txt'
+delux_uri = 'matilda_files/Delux.txt'
+total_uri = 'matilda_files/Total.txt'
+
+revenue_graphs(basic_uri)
+revenue_graphs(delux_uri)
+revenue_graphs(total_uri)
